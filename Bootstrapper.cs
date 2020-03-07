ef97a2c2d5d8054f62a3369da223b238be835763
Austin Rafuls <austin.rafuls@gmail.com>
2020-03-07 13:41:17 -0500

    Add project files.

---
+++ Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+using FinanceCalculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FinanceCalculator
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+
+        /*
+        NAME
+            protected override void OnStartup(object sender, StartupEventArgs e)
+
+        PARAMETERS
+            object sender       - A reference to the sender object.
+            StartupEventArgs e  - A reference to any additional arguments.
+
+        SUMMARY
+            Customized OnStartup behavior by overriding the BootstrapperBase in
+            CaliburnMicro.
+        */
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
